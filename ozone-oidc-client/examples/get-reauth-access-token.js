9d5289b0c7441185e0a7fbd86996837595eb5633
Raj <rajeshc7@gmail.com>
2020-02-08 14:56:09 +0000

    updated oidc client

--- ozone-oidc-client/examples/get-reauth-access-token.js
+++ ozone-oidc-client/examples/get-reauth-access-token.js
@@ -3,7 +3,9 @@ const clientConfig = require('./config/client-config-local.json');
 
 async function go() {
   const oidcClient = new OidcClient(clientConfig);
-  return oidcClient.getTokenByJWTGrant('accounts openid',"aac-7b6b91e5-61ae-40a9-b42c-755108f4f3ff");
+  const args = process.argv.slice(2);
+  const intentID = args[0];
+  return oidcClient.getTokenByJWTGrant('accounts openid', intentID);
 }
 
 go()
