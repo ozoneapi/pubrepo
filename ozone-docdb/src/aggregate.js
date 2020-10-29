c70921359d5064b44ed550aec4f14d8e89833bf8
David Plews <david@ozoneapi.com>
2020-10-29 12:08:53 +0000

    Aggregate class

---
+++ ozone-docdb/src/aggregate.js
@@ -0,0 +1,67 @@
+const { MongoClient } = require('mongodb');
+
+class Aggregate {
+  constructor(connectString, collection) {
+    this.connectString = connectString;
+    this.collection = collection;
+  }
+
+  static async getInput() {
+    return new Promise(((resolve, reject) => {
+      const { stdin } = process;
+      let data = '';
+
+      stdin.setEncoding('utf8');
+      stdin.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      stdin.on('end', () => {
+        resolve(data);
+      });
+
+      stdin.on('error', reject);
+    }));
+  }
+
+  async initMongo() {
+    // init mongo
+    const client = await MongoClient.connect(this.connectString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    this.db = client.db();
+  }
+
+  async go() {
+    const aggregation = await Aggregate.getInput();
+
+    await this.initMongo();
+
+    const out = await this.db.collection(this.collection).aggregate(aggregation).toArray();
+
+    console.log(aggregation);
+    console.log(this.connectString);
+    console.log(this.collection);
+    console.log(out);
+  }
+}
+
+async function go() {
+  if (process.argv.length !== 4) {
+    throw new Error('2 params - cnnect string and collection ');
+  }
+  console.log(process.argv[2])
+  console.log(process.argv[3])
+  const aggregate = new Aggregate(process.argv[2], process.argv[3]);
+  return aggregate.go();
+}
+
+go()
+  .then(() => console.log('script complete'))
+  .catch((err) => {
+    console.log('script failed');
+    console.log(err);
+    process.exit(-1);
+  });
